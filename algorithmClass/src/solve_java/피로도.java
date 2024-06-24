f4bda299a1f61377440e36acd147d2cd9ce93aa2
H-Yeji <yeji980603@naver.com>
2024-06-25 00:53:34 +0900

    update algrithm class

--- algorithmClass/src/solve_java/피로도.java
+++ algorithmClass/src/solve_java/피로도.java
@@ -1,45 +1,35 @@
 package solve_java;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+// ⭐⭐⭐ 백트래킹/dfs
 public class 피로도 {
 
-    static List<int[]> adjList;
-    static List<Integer> tmp;
-    static List<List<int[]>> result;
-    static int[] visited;
+    static boolean[] visited;
+    static int cnt = 0;
+    public static void dfs(int depth, int k, int[][] dun) {
 
-    public static void check(List<int[]> tmp, int[][] dun, int k) {
-
-        if (tmp.size() == dun.length) {
-            result.add(new ArrayList<>(tmp));
-        }
         for (int i = 0; i < dun.length; i++) {
-            tmp.add(dun[i]);
-            check(tmp, dun, k);
-            tmp.remove(tmp.size() - 1);
+            if (visited[i] || dun[i][0] > k) {
+                continue;
+            }
+            visited[i] = true;
+            dfs(depth + 1, k - dun[i][1], dun);
+            visited[i] = false;
         }
+        cnt = Math.max(cnt, depth);
     }
+
     public static int solution(int k, int[][] dun) {
-        int answer = -1;
 
-        adjList = new ArrayList<int[]>();
-        for (int i = 0; i < dun.length; i++) {
-            adjList.add(dun[i]);
-        }
-
-
-
-        return answer;
+        visited = new boolean[dun.length];
+        dfs(0, k, dun);
+        return cnt;
     }
 
     public static void main(String[] args) {
 
         int k = 80; // 현재 피로도
         int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
-        solution(k, dungeons);
+        System.out.println(solution(k, dungeons));
 
     }
 }
