3e694ae3e92f0eed58bfc057ea62b9f745d0a013
H-Yeji <yeji980603@naver.com>
2024-06-29 02:25:30 +0900

    up to 6/29 - 브루트포스 알고리즘 문제

---
+++ solve_java/src/누적합/BOJ2003.java
@@ -0,0 +1,43 @@
+package 누적합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2003 {
+
+    // 수들의 합2 - 투포인터, 누적합, 브루트포스 알고리즘
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // n, m
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0; int end = 0;
+        int cnt = 0; int sum = 0;
+
+        while (true) {
+            if (sum >= m) {
+                sum -= arr[start++];
+            } else if (end == n) {
+                break;
+            } else {
+                sum += arr[end++];
+            }
+
+            if (sum == m) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
+
